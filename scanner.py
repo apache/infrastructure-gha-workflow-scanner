9e8a7ef0072425bb51d75ba9c5aa603c081a37c8
Drew Foulks <dfoulks@apache.org>
2024-04-02 10:32:04 -0400

    Added optional config file option to scanner.

--- scanner.py
+++ scanner.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 import yaml
 import gha_scanner
+import argparse
+
+parser = argparse.ArgumentParser(description="Start the GHA Scanner")
+parser.add_argument('-c', '--config', help="Alternate Config", default="gha_scanner.config")
+args = parser.parse_args()
 
 if __name__ == "__main__":
-    gh = gha_scanner.Scanner(yaml.safe_load(open("gha_scanner.config", "r").read()))
+    gh = gha_scanner.Scanner(yaml.safe_load(open(args.config, "r").read()))
     gh.scan()
