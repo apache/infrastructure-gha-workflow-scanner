7b0a73236a7e3221eb773530fe9a61073ea17dd5
Drew Foulks <dfoulks@apache.org>
2024-12-04 13:10:01 -0500

    typo in the default config file name

--- scanner.py
+++ scanner.py
@@ -4,7 +4,7 @@ import gha_scanner
 import argparse
 
 parser = argparse.ArgumentParser(description="Start the GHA Scanner")
-parser.add_argument('-c', '--config', help="Alternate Config", default="gha_workflow_scanner.yaml")
+parser.add_argument('-c', '--config', help="Alternate Config", default="gha-workflow-scanner.yaml")
 args = parser.parse_args()
 
 if __name__ == "__main__":
